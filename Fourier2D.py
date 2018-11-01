2a485aadfc3e63bcfd5eaf430103c9839520347c
tonnygualdron <42391447+tonnygualdron@users.noreply.github.com>
2018-10-31 22:45:10 -0500

    Version final
    Realiza todos los requerimientos

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,39 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import fftpack as f
-from scipy.stats import mode
 
 
 #abre la imagen la convierte en una matrix de flotantes
 Arboles = plt.imread('Arboles.png').astype(float)
+#de no estar los valores entre 0 y 1 los normaliza
+Arboles = Arboles/np.amax(Arboles)
 #calcula la transformada
-ArbolesFFT = f.fft2(Arboles)
-#la centra
-ArbolesFFT2 = f.fftshift(ArbolesFFT)
-#Calcula las magnitudes y saca logaritmo para lograr una escala mejor
-NormaFFT = np.log(np.abs(ArbolesFFT2))
-#Genera la grafica con las magnitudes centradas de la transformada en escala de grises
-plt.imshow(NormaFFT, cmap = 'gray')
-plt.colorbar()
+ArbolesFFT =f.fft2(Arboles)
+#centra la transformada
+ArbolesFFT2=f.fftshift(ArbolesFFT)
+#calcula el valor de la transformad y luego la centra
+plt.imshow(f.fftshift(abs(ArbolesFFT)),cmap = 'gray')
 plt.title("Transformada de Fourier")
 plt.savefig("GualdronTonny_FT2D.pdf")
 plt.close()
 
-ArbolesFFT4=np.ones((l)).astype(complex)
-for i in range(np.shape(ArbolesFFT)[0]):
-     ArbolesFFT3[i]=ArbolesFFT[i,i]
+# de la grafica anterior se notan que ha cuatro puntos que son mas tenues que el central, es decir que el central correponde al la imagen y los otros al ruido entonces deberiamos volver esos puntos ceros
+#numero de lineas
+l=np.shape(Arboles)[0]
+#numero de columnas
+c=np.shape(Arboles)[1]
+#Crea una matriz del mismo tamaño con solo unos
+frecuencias = np.ones((l,c)).astype(complex)
+#Busca las 5 posiciones mas brillantes en orden de intensidad
+copia=f.fftshift(abs(ArbolesFFT))
+ii=[]
+jj=[]
+ia=0
+ja=0
+m=copia[0][0]
+for z in range(5):    
+    for i in range(l):
+        for j in range(c):
+            if(copia[i,j]>m):
+                m=copia[i,j]
+                ia=i
+                ja=j
+    copia[ia,ja]=0
+    ii.append(ia)
+    jj.append(ja)
+    ia=0
+    ja=0
+    m=copia[0][0]
 
-moda=mode(ArbolesFFT)
-l=np.shape(ArbolesFFT)[0]
-c=np.shape(ArbolesFFT)[1]
-ArbolesFFT3=np.ones((l,c)).astype(complex)
-for j in range(np.shape(ArbolesFFT)[1]):
-    for i in range(np.shape(ArbolesFFT)[0]):
-          ArbolesFFT3[i,j]=ArbolesFFT[i,j]+moda[0][0][j]
-ArbolesFil=f.ifft2(ArbolesFFT3).real
-plt.imshow(ArbolesFil, cmap = 'gray')
-plt.show()
-ArbolesFinal=Arboles-ArbolesFil
-plt.imshow(ArbolesFinal, cmap = 'gray')
-plt.show()
+#convierte los 4 puntos mas brillantes despues del maximo en 0
+frecuencias[ii[1],jj[1]]=0
+frecuencias[ii[2],jj[2]]=0
+frecuencias[ii[3],jj[3]]=0
+frecuencias[ii[4],jj[4]]=0
+#Crea una matriz donde se guardara el resultado
+nueva = np.ones((l,c)).astype(complex)
+#Se multiplica uno a uno para volver solo esos cuatro puntos 0
+for i in range(l):
+    for j in range(c):
+         nueva[i,j]=ArbolesFFT2[i,j]*frecuencias[i,j]
+#Halla de nuevo la imagen
+nueva2=f.ifft2(f.ifftshift(nueva))
+plt.imshow(abs(nueva),cmap = 'gray')
+plt.title("Transformada de Fourier Filtrada")
+plt.savefig("GualdronTonny_FT2D_filtrada.pdf")
+plt.close()
 
+#verifica que las posiciones de los 4 puntos ruidos sean 0
+if(nueva[ii[1],jj[1]]==0 and nueva[ii[2],jj[2]]==0 and nueva[ii[3],jj[3]]==0 and nueva[ii[4],jj[4]]==0):
+    plt.imshow(abs(nueva2),cmap = 'gray')
+    plt.title("Imagen Final")
+    plt.savefig("GualdronTonny_Imagen_filtrada.pdf")
+    plt.close()
+else:
+    print("No se pudo eliminar el ruido") 
