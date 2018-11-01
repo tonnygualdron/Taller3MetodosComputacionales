5b6b201a11b5e363f8f3cbf16d120b6f167c4440
tonnygualdron <42391447+tonnygualdron@users.noreply.github.com>
2018-10-31 20:43:44 -0500

    Primer intento de filtro Fallido

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,34 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as pltc
 from scipy import fftpack as f
+from scipy.stats import mode
+
+
 #abre la imagen la convierte en una matrix de flotantes
 Arboles = plt.imread('Arboles.png').astype(float)
-
+#calcula la transformada
 ArbolesFFT = f.fft2(Arboles)
-ArbolesFFTNorma= np.abs(ArbolesFFT)
-minA= np.amin(np.abs(ArbolesFFT))
-maxA= np.amax(np.abs(ArbolesFFT))
-plt.imshow(ArbolesFFTNorma, norm=pltc.LogNorm(vmin=minA,vmax=maxA))
+#la centra
+ArbolesFFT2 = f.fftshift(ArbolesFFT)
+#Calcula las magnitudes y saca logaritmo para lograr una escala mejor
+NormaFFT = np.log(np.abs(ArbolesFFT2))
+#Genera la grafica con las magnitudes centradas de la transformada en escala de grises
+plt.imshow(NormaFFT, cmap = 'gray')
 plt.colorbar()
+plt.title("Transformada de Fourier")
+plt.savefig("GualdronTonny_FT2D.pdf")
+plt.close()
+
+ArbolesFFT4=np.ones((l)).astype(complex)
+for i in range(np.shape(ArbolesFFT)[0]):
+     ArbolesFFT3[i]=ArbolesFFT[i,i]
+
+moda=mode(ArbolesFFT)
+l=np.shape(ArbolesFFT)[0]
+c=np.shape(ArbolesFFT)[1]
+ArbolesFFT3=np.ones((l,c)).astype(complex)
+for j in range(np.shape(ArbolesFFT)[1]):
+    for i in range(np.shape(ArbolesFFT)[0]):
+          ArbolesFFT3[i,j]=ArbolesFFT[i,j]+moda[0][0][j]
+ArbolesFil=f.ifft2(ArbolesFFT3).real
+plt.imshow(ArbolesFil, cmap = 'gray')
+plt.show()
+ArbolesFinal=Arboles-ArbolesFil
+plt.imshow(ArbolesFinal, cmap = 'gray')
 plt.show()
 
-k=0.1
-CopiaArboles=ArbolesFFT.copy()
-l=np.shape(CopiaArboles)[0]
-c=np.shape(CopiaArboles)[1]
-
-CopiaArboles[int(l*k):int(l*(1-k))]=1
-CopiaArboles[:,int(c*k):int(c*(1-k))]=1
-
-ArbolesFFTNorma2= np.abs(CopiaArboles)
-plt.imshow(ArbolesFFTNorma2, norm=pltc.LogNorm(vmin=minA,vmax=maxA))
-plt.colorbar()
-plt.show()
-
-ArbolesFil = f.ifft2(CopiaArboles).real
-
-plt.figure()
-plt.imshow(ArbolesFil, plt.cm.gray)
-plt.title('Reconstructed Image')
-plt.show()
